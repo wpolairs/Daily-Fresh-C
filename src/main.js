7b33d8d38e8795206a62f276a0ecb3c53baf7761
wpolair <wpolairs@gmail.com>
2021-02-21 21:29:32 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import Vant from 'vant';
+import 'vant/lib/index.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'lib-flexible';
 
 Vue.config.productionTip = false;
+Vue.use(Vant);
 
 new Vue({
   router,
